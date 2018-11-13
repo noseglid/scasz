fdedc178e2482ec1bd0bbac8c0784136ec68071b
Alexander Olsson <noseglid@gmail.com>
2018-11-13 21:02:09 +0100

    Improved Login
    Go to app directly if available.

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,20 +1,46 @@
 import './login.scss';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import { stringify } from 'querystring';
+import { withSpotifyContext } from './SpotifyContext';
+import { SpotifyBindings } from './SpotifyBindings';
+import { History } from 'history';
 
 const onLogin = () => {
   const qs = stringify({
     response_type: 'token',
     client_id: '602140cd526e461e86264f40d1c4435e',
     scope: 'streaming user-read-birthdate user-read-email user-read-private user-top-read',
-    redirect_uri: 'http://localhost:1234/authorized',
+    redirect_uri: `http://${window.location.host}/authorized`,
     state: '',
   });
   window.location.assign(`https://accounts.spotify.com/authorize?${qs}`);
 };
 
-export const Login: FunctionComponent = () => (
-  <div className="login">
-    <button onClick={onLogin}>Login</button>
-  </div>
-);
+interface Props {
+  spotifyBindings: SpotifyBindings;
+  history: History;
+}
+
+const Login: FunctionComponent<Props> = ({ spotifyBindings, history }) => {
+  useEffect(() => {
+    async function initialize() {
+      try {
+        await spotifyBindings.me();
+        console.log('pushing', history);
+        history.push('/i');
+      } catch (e) {
+        // Not logged in, do nothing
+      }
+    }
+    initialize();
+  }, []);
+
+  return (
+    <div className="login">
+      <button onClick={onLogin}>Login</button>
+    </div>
+  );
+};
+
+const LoginWithContext = withSpotifyContext(Login);
+export { LoginWithContext as Login };
